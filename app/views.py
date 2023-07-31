33b984279e1e3bf4700f1e87e31b05f2a8c15d65
ashwinikumar96 <ashwinikumardash96@gmail.com>
2023-07-31 11:33:10 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def data_render(request):
-    return render(request,'data_render.html',context={'name':'Ashwin', 'age':23})
+    return render(request,'data_render.html',context={'name':'Ashwin', 'age':23, 'hobbies':['cricket','volly']})
 def jinja_operations(request):
     return render(request,'jinja_operations.html',context={'a':1000, 'b': 5000, 'c': 200})
